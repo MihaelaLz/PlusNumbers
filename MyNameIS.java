e12d7580ca1e14617b9052218fe898fe7fdf9c13
MihaelaLz <100777329+MihaelaLz@users.noreply.github.com>
2022-03-10 16:31:42 +0200

    Add files via upload

---
+++ MyNameIS.java
@@ -0,0 +1,12 @@
+package mihaela;
+
+import java.util.Locale;
+
+public class MyNameIS {
+    public static void main(String[] args) {
+        String myName = "Mihaela";
+        System.out.println(myName.length());
+        System.out.println(myName.toUpperCase());
+        System.out.print(myName.toLowerCase());
+    }
+}
